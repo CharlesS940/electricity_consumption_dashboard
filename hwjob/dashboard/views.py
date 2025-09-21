cd90997ab6c54bc47e5cf18bbadbea8630ff0214
Charles Siret <charles.siret.cs@orange.fr>
2025-09-21 14:38:52 +0200

    Compute electric heating on request

--- hwjob/dashboard/views.py
+++ hwjob/dashboard/views.py
@@ -8,8 +8,17 @@ def consumption_view(request, client_id):
     """
     queryset = Consumption.objects.filter(client__id=client_id).order_by("-year", "-month")[:12]
     consumptions = list(reversed(queryset))
+    
+    summer_consumption = sum(c.kwh_consumed for c in consumptions if c.month in [6, 7, 8])
+    winter_consumption = sum(c.kwh_consumed for c in consumptions if c.month in [12, 1, 2])
+    if winter_consumption > 2 * summer_consumption:
+        electric_heating = True
+    else:
+        electric_heating = False
     context = {
-        "consumptions": consumptions,}
+        "consumptions": consumptions,
+        "electric_heating": electric_heating
+    }
     return render(request, "dashboard/consumption_detail.html", context)
 
 

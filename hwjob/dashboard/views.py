c23d048cd3f4e094720bc19877c24ed89de3b78a
Charles Siret <charles.siret.cs@orange.fr>
2025-09-21 18:17:08 +0200

    Refactor: use database values in view, compute if missing

--- hwjob/dashboard/views.py
+++ hwjob/dashboard/views.py
@@ -1,23 +1,27 @@
 from django.shortcuts import render
-from dashboard.models import Consumption
+from dashboard.models import Consumption, Client
+from dashboard.utils import has_electric_heating, find_anomalies
 
 
 def consumption_view(request, client_id):
     """
-    Retrieving the last 12 months of consumption for a client
+    Retrieving the last 12 months of consumption for a client for display. If the client's electric heating status or anomalies are not set, they are computed and saved.
     """
+    client = Client.objects.get(id=client_id)
     queryset = Consumption.objects.filter(client__id=client_id).order_by("-year", "-month")[:12]
     consumptions = list(reversed(queryset))
+
+    if client.has_elec_heating is None:
+        client = has_electric_heating(client, consumptions, threshold=2)
+        client.save()
+
+    if any(c.has_anomaly is None for c in consumptions):
+        consumptions = find_anomalies(client, consumptions)
+        Consumption.objects.bulk_update(consumptions, ['has_anomaly'])
     
-    summer_consumption = sum(c.kwh_consumed for c in consumptions if c.month in [6, 7, 8])
-    winter_consumption = sum(c.kwh_consumed for c in consumptions if c.month in [12, 1, 2])
-    if winter_consumption > 2 * summer_consumption:
-        electric_heating = True
-    else:
-        electric_heating = False
     context = {
         "consumptions": consumptions,
-        "electric_heating": electric_heating
+        "has_electric_heating": client.has_elec_heating,
     }
     return render(request, "dashboard/consumption_detail.html", context)
 

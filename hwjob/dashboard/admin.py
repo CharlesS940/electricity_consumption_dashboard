4b612eba6ac1db3c11def32fc48e0b9772d5caf7
Charles Siret <charles.siret.cs@orange.fr>
2025-09-22 16:07:04 +0200

    Implement filtering by electric heating and health status in admin page

--- hwjob/dashboard/admin.py
+++ hwjob/dashboard/admin.py
@@ -8,12 +8,24 @@ from dashboard.models import Client
 
 class ClientsListView(ListView):
     """
-    A list of clients
+    Returns list of clients, with optional filtering by electric heating and anomaly status.
     """
-
-    queryset = Client.objects.all()
     context_object_name = "clients_list"
     template_name = "dashboard/clients_list.html"
+
+    def get_queryset(self):
+        queryset = Client.objects.all()
+        elec_heating = self.request.GET.get("electric_heating")
+        anomaly_status = self.request.GET.get("anomaly_status")
+        if elec_heating == "yes":
+            queryset = queryset.filter(has_elec_heating=True)
+        elif elec_heating == "no":
+            queryset = queryset.filter(has_elec_heating=False)
+        if anomaly_status == "normal":
+            queryset = queryset.filter(has_recent_anomaly=False)
+        elif anomaly_status == "recent_anomaly":
+            queryset = queryset.filter(has_recent_anomaly=True)
+        return queryset
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -21,6 +33,8 @@ class ClientsListView(ListView):
             "title": "Clients list",
             "description": "Browse which clients has an electrical heating or an anomaly",
         }
+        context["selected_electric_heating"] = self.request.GET.get("electric_heating", "")
+        context["selected_anomaly_status"] = self.request.GET.get("anomaly_status", "")
         return context
 
 

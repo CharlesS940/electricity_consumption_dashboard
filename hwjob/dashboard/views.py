368994b6516c980155ca424283ecbd0c2bf0bd10
Charles Siret <charles.siret.cs@orange.fr>
2025-09-22 13:33:53 +0200

    Display full anomaly history in dashboard

--- hwjob/dashboard/views.py
+++ hwjob/dashboard/views.py
@@ -9,8 +9,8 @@ def consumption_view(request, client_id):
     Retrieving the last 12 months of consumption for a client for display. If the client's electric heating status or anomalies are not set, they are computed and saved.
     """
     client = Client.objects.get(id=client_id)
-    queryset = Consumption.objects.filter(client__id=client_id).order_by("-year", "-month")[:12]
-    consumptions = list(reversed(queryset))
+    queryset = Consumption.objects.filter(client__id=client_id).order_by("-year", "-month")
+    consumptions = list(reversed(queryset[:12]))
 
     if client.has_elec_heating is None:
         client = has_electric_heating(client, consumptions, threshold=2)
@@ -19,10 +19,13 @@ def consumption_view(request, client_id):
     if any(c.has_anomaly is None for c in consumptions):
         consumptions = find_anomalies(client, consumptions)
         Consumption.objects.bulk_update(consumptions, ['has_anomaly'])
+
+    anomalies = queryset.filter(has_anomaly=True)
     
     context = {
         "consumptions": consumptions,
         "has_electric_heating": client.has_elec_heating,
+        "anomalies": anomalies,
     }
     return render(request, "dashboard/consumption_detail.html", context)
 
